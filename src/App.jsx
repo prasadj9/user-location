1c1a84ec874c9376a3aa4130e45d8b766e3f93f9
prasadj9 <prasad.jambhale@nimapinfotech.com>
2025-06-18 10:39:38 +0530

    show user full address

--- src/App.jsx
+++ src/App.jsx
@@ -2,23 +2,45 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const apiEndpoint = import.meta.env.VITE_APP_BASE_URL;
+  const apikey = import.meta.env.VITE_APP_API_KEY;
   const [msg, setMsg] = useState("");
+  const [address, setAddress] = useState("");
+
+  const getUserAddress = async (latitude, longitude) => {
+
+    let query = `${latitude},${longitude}`;
+    let apiUrl = `${apiEndpoint}?key=${apikey}&q=${query}&pretty=1`;
+
+    try {
+      const res = await fetch(apiUrl);
+      const data = await res.json();
+      setAddress(data?.results[0]?.formatted)
+      console.log('data', data?.results[0]?.formatted)
+    } catch (error) {
+      console.log('error', error)
+    }
+  };
 
   const getLocation = () => {
-      navigator.geolocation.getCurrentPosition((pos) => {
+    navigator.geolocation.getCurrentPosition(
+      (pos) => {
         const { latitude, longitude } = pos.coords;
-        console.log("pos", latitude, longitude);
-        setMsg(`${latitude}, ${longitude}`)
-      }, (error) => {
-        console.log('error', error)
-        setMsg(error.message)
-      });
-  }
+        getUserAddress(latitude, longitude)
+        setMsg(`${latitude}, ${longitude}`);
+      },
+      (error) => {
+        console.log("error", error);
+        setMsg(error.message);
+      }
+    );
+  };
 
   return (
     <>
-      <button onClick={getLocation} >Show user Location</button>
+      <button onClick={getLocation}>Show user Location</button>
       <p>{msg}</p>
+      {address && <p>Address : {address}</p>}
     </>
   );
 }
